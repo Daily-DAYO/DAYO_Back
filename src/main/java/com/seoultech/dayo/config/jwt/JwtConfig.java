5091b5ab1ead74b802a4883519251ecc2fd7e6bc
jdyj <41135099+jdyj@users.noreply.github.com>
2022-01-06 17:27:29 +0900

    Delete JwtConfig.java

--- src/main/java/com/seoultech/dayo/config/jwt/JwtConfig.java
+++
@@ -1,6 +0,0 @@
-package com.seoultech.dayo.config.jwt;
-
-public class JwtConfig {
-
-    public static final String JWT_SECRET = "c3ByaW5nLWFzb2R2b2stcXdrZXJva3p4Y3YtZGF5by1hZGtmb3Nka2ZzcHJpbmctc2tvc2RrZm9renhvY3ZreHpvY3Z4dnhvY3YK";
-}
