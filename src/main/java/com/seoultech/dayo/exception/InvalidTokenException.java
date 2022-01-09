edb1ce7374d06926693e6bf460158f3053682850
jdyj <jdyj@naver.com>
2022-01-09 22:00:57 +0900

    [ADD] Token 에러 핸들링

---
+++ src/main/java/com/seoultech/dayo/exception/InvalidTokenException.java
@@ -0,0 +1,10 @@
+package com.seoultech.dayo.exception;
+
+public class InvalidTokenException extends RuntimeException {
+
+    public static final String MESSAGE = "유효하지 않은 토큰입니다.";
+
+    public InvalidTokenException() {
+        super(MESSAGE);
+    }
+}
