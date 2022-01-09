9966adbbed45af7017eed83fbd3edef3e8f7570e
jdyj <jdyj@naver.com>
2022-01-09 22:00:38 +0900

    [ADD] Hashtag elasticsearch 추가

---
+++ src/main/java/com/seoultech/dayo/config/elasticsearch/ElasticSearchConfig.java
@@ -0,0 +1,22 @@
+package com.seoultech.dayo.config.elasticsearch;
+
+import com.seoultech.dayo.hashtag.repository.HashtagSearchRepository;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.client.ClientConfiguration;
+import org.springframework.data.elasticsearch.client.RestClients;
+import org.springframework.data.elasticsearch.config.AbstractElasticsearchConfiguration;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+
+@EnableElasticsearchRepositories(basePackageClasses = {HashtagSearchRepository.class})
+@Configuration
+public class ElasticSearchConfig extends AbstractElasticsearchConfiguration {
+
+    @Override
+    public RestHighLevelClient elasticsearchClient() {
+        ClientConfiguration configuration = ClientConfiguration.builder()
+                .connectedTo("localhost:9200")
+                .build();
+        return RestClients.create(configuration).rest();
+    }
+}
