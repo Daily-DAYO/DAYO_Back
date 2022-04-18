905c75c0fadd17181d8b16082892cee5f0a44ff3
jdyj <jdyj@naver.com>
2022-04-18 17:44:25 +0900

    [ADD] 검색 service 생성

---
+++ src/main/java/com/seoultech/dayo/search/service/SearchService.java
@@ -0,0 +1,12 @@
+package com.seoultech.dayo.search.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class SearchService {
+
+}
