eb34a653d0eb2d84c99b58e03046a557effc95c9
jdyj <jdyj@naver.com>
2022-04-18 17:44:20 +0900

    [ADD] 검색 repository 생성

---
+++ src/main/java/com/seoultech/dayo/search/repository/SearchRepository.java
@@ -0,0 +1,8 @@
+package com.seoultech.dayo.search.repository;
+
+import com.seoultech.dayo.search.Search;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SearchRepository extends JpaRepository<Search, Long> {
+
+}
