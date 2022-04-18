3eab2b2b6e7be9872329bd8425315cf578678ba1
jdyj <jdyj@naver.com>
2022-04-18 17:44:14 +0900

    [ADD] 검색 controller 생성

---
+++ src/main/java/com/seoultech/dayo/search/controller/SearchController.java
@@ -0,0 +1,12 @@
+package com.seoultech.dayo.search.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/search")
+public class SearchController {
+
+}
