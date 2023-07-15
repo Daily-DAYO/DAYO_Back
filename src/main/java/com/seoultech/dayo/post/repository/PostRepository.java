a53bb4badd32c2e0629c865fe38889d46fe4fb08
jdyj <jdyj@naver.com>
2023-07-15 16:30:19 +0900

    [FIX] post 프라이버시 변경

--- src/main/java/com/seoultech/dayo/post/repository/PostRepository.java
+++ src/main/java/com/seoultech/dayo/post/repository/PostRepository.java
@@ -3,12 +3,11 @@ package com.seoultech.dayo.post.repository;
 import com.seoultech.dayo.member.Member;
 import com.seoultech.dayo.post.Category;
 import com.seoultech.dayo.post.Post;
+import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-
-import java.util.List;
-import java.util.Optional;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
 
@@ -21,7 +20,7 @@ public interface PostRepository extends JpaRepository<Post, Long> {
   @Query("select p from Post p join fetch p.comments where p.id = :id")
   Optional<Post> findByIdWithComment(@Param("id") Long id);
 
-  @Query("select p from Post p join fetch p.member where p.category = :category")
+  @Query("select p from Post p join fetch p.member where p.category = :category and p.privacy = 'ALL'")
   List<Post> findAllByCategoryUsingJoinMember(@Param("category") Category category);
 
   @Query("select p from Post p join fetch p.member")
